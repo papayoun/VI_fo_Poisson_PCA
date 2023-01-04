d542053a35ff6e9e71da34c9230dd2d4ba2a9422
papayoun <pierre.gloaguen@gmail.com>
2023-01-04 18:37:38 +0100

    ajout de l'actu avec effet fixe dans l'exemple

--- script_PPCA_VI_implementation_fixed.R
+++ script_PPCA_VI_implementation_fixed.R
@@ -12,7 +12,6 @@ Y <- read.table("data_PPCA.txt", sep = ";") %>%
   as.matrix()
 X <- read.table("fixed_PPCA.txt", sep = ";") %>%
   as.matrix()
-XprimeX <- t(X) %*% X # Once for all
 F_x <- ncol(X)
 
 # VI inference functions -----------------------------------------------------
@@ -30,9 +29,12 @@ priors <- list(Sigma = list(A = 3, B = 2),
                Delta= list(A = c(5, rep(2, q - 1)), 
                            B = 1),
                Beta = list(M = rep(0, F_x),
-                           C = rep(100, F_x)))
-get_result <- function(seed, n_steps){
+                           C = rep(0.01, F_x)))
+get_result <- function(Y, X, seed, n_steps){
   set.seed(seed)
+  if(is.null(X)){
+    F_x = 1
+  }
   init_params <- list(Lambda = list(M = matrix(rnorm(p * q),
                                                nrow = q, ncol = p),
                                     Cov = array(diag(1, q), 
@@ -52,17 +54,18 @@ get_result <- function(seed, n_steps){
                       Phi = list(A =  matrix(priors$Phi$A + 0.5, p, q),
                                  B = matrix(runif(p * q, 1, 3), p, q))) 
   
-  result <- get_CAVI(data_ = Y, q = q, 
+  result <- get_CAVI(data_ = Y, X = X, q = q, 
                      n_steps = n_steps, params = init_params,
                      updates = c(Lambda = TRUE, Sigma = TRUE,
                                  Eta = TRUE, Delta = TRUE, 
-                                 Phi = TRUE, Beta = FALSE),
+                                 Phi = TRUE, Beta = !is.null(X)),
                      priors = priors)
   result
 }
 
 all_results <- mclapply(1:4, get_result, 
-                        n_steps = 2000, 
+                        Y = Y, X = X,
+                        n_steps = 200, 
                         mc.cores = detectCores())
 
 
