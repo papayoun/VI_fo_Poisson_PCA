7ad05abc9fd23d2209737d753f316acce56b590d
rempart <eric.parent@agroparistech.fr>
2023-03-17 12:02:47 +0100

    essais PPPCA VI ok
    progress bar  de pacman ne marche pas...
    Du coup j'ai bricolé mon propre progress bar

--- script_Poisson_VI_implementation.R
+++ script_Poisson_VI_implementation.R
@@ -2,7 +2,9 @@ rm(list = ls())
 library(tidyverse)
 library(abind) # To gather results together
 library(parallel) # For some parallel computations
-library(pacman) # For progress bar
+# library(pacman) # For progress bar (ne marche pas... ou travail non fini par celui qui l'a ajouté!
+# Du coup j'ai bricolé mon propre progress bar Eric)
+
 # source("utils_Poisson_PPCA_generating_data.R") # For true values
 
 
@@ -24,13 +26,20 @@ result <- get_CAVI(Y = Y,
                    n_steps = 50, 
                    debug = FALSE)
 
-plot(result$ELBOS[-c(1:90),])
+
+plot(result$ELBOS[-c(1:2),])
 result$params$Lambda$M
 result_VI <- result
 save(result_VI, file = "result_VI.RData")
 all_results <- lapply(1:4,
                         FUN = function(i)
-                          get_result(Y = Y, X = X_data, n_steps = 50, seed = i)
+                      #    get_result(Y = Y, X = X_data, n_steps = 50, seed = i)
+                          get_CAVI(Y = Y, 
+                                   X = X,
+                                   q = 7,
+                                   seed = i, 
+                                   n_steps = 150, 
+                                   debug = FALSE)
                       )
 
 map_dfr(all_results, "ELBOS", .id = "Replicate") %>%
